925b215b121523869ada0963ebb0a3d7e5706348
matt <85061655+Mattluna@users.noreply.github.com>
2021-06-25 14:16:33 -0400

    Add files via upload

---
+++ Integrating Languages2/Release/PythonCode.py
@@ -0,0 +1,36 @@
+import re
+import string
+
+   
+def MultiplicationTable(v):
+    b=0
+    b=v*1
+    print(int(v), " x  1 = ", int(b)) 
+    b=v*2
+    print(int(v), " x  2 = ", int(b)) 
+    b=v*3
+    print(int(v), " x  3 = ", int(b)) 
+    b=v*4
+    print(int(v), " x  4 = ", int(b)) 
+    b=v*5
+    print(int(v), " x  5 = ", int(b)) 
+    b=v*6
+    print(int(v), " x  6 = ", int(b)) 
+    b=v*7
+    print(int(v), " x  7 = ", int(b)) 
+    b=v*8
+    print(int(v), " x  8 = ", int(b)) 
+    b=v*9
+    print(int(v), " x  9 = ", int(b)) 
+    b=v*10
+    print(int(v), " x 10 = ", int(b)) 
+       
+    return b;
+    
+def DoubleValue(v):
+    return v*2;
+
+
+
+
+    
